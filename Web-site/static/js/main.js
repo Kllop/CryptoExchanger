85276fd8f11889c2ad4aa65df819a17cfc907122
niruin <87806883+niruin@users.noreply.github.com>
2023-08-04 19:05:38 +0300

    feat(layout): add pages

--- Web-site/static/js/main.js
+++ Web-site/static/js/main.js
@@ -1,11 +1,13 @@
-url = "http://127.0.0.1:5010"
-url_data = "http://127.0.0.1:9000"
+import {updateSelectStyle} from "./utils/select.js";
 
-offers_data = {}
-course_data = {}
+var url = "http://127.0.0.1:5010"
+var url_data = "http://127.0.0.1:9000"
+
+var offers_data = {}
+var course_data = {}
 
 function UpdateSetterOffers(setterName) {
-  values = offers_data[setterName]
+  var values = offers_data[setterName]
   if (values == null) {
     return
   }
@@ -14,7 +16,7 @@ function UpdateSetterOffers(setterName) {
     const option1 = $("<option>").attr('value', unit).text(values[unit]);
     $('#setter').append(option1);
   }
-  defaultValue = "Tinkoff"
+  var defaultValue = "Tinkoff"
   $('#setter').val(defaultValue)
   updateSelectStyle();
 }
@@ -40,7 +42,7 @@ $('#getter_value').on('change', function () {
 $("#form-send-bid-data").on('submit', function (event) {
   event.preventDefault();
   if (CheckAMLPolitic()) {
-    window.location.href = '#/bid';
+    SenderOfferData()
   } else {
     alert("Примите AML политику")
   }
@@ -75,7 +77,7 @@ function SenderOfferData() {
       getterNumber: getterNumber
     }),
     success: function (response) {
-      document.getElementById("main").innerHTML = response;
+      window.location.href = '/bid'
     },
     error: function (xhr) {
       console.log("Error load fiat data", xhr)
@@ -85,11 +87,11 @@ function SenderOfferData() {
 
 function UpdateGetterOffers() {
   $('#getter').empty();
-  defaultValue = "BTC"
   for (let unit of Object.keys(offers_data)) {
     const option1 = $("<option>").attr('value', unit).text(unit);
     $('#getter').append(option1);
   }
+  var defaultValue = "BTC"
   $('#getter').val(defaultValue)
   UpdateSetterOffers(defaultValue)
 }
@@ -107,8 +109,8 @@ function CalculationCoinRate() {
 }
 
 function ChangeCourse() {
-  getter_value = $('#getter').val()
-  setter_value = $('#setter').val()
+  var getter_value = $('#getter').val()
+  var setter_value = $('#setter').val()
   var price = course_data[getter_value][setter_value]
   return price
 }
@@ -147,81 +149,5 @@ function UpdateCourse() {
 
 GetOffers()
 UpdateCourse()
-init()
 
 setInterval(UpdateCourse, 15000)
-
-window.addEventListener('hashchange', function() {
- route();
-});
-
-function route() {
-  const hash = window.location.hash;
-  if (hash === '#/bid') {
-    SenderOfferData();
-  }
-}
-
-function init() {
-  route();
-}
-
-$(document).on('click', '#btn-bid-accept', function (event) {
-  alert("Заявка в обработке");
-  event.target.parentElement.style.display = 'none';
-});
-$(document).on('click', '#btn-bid-cancel', function (event) {
-  alert("Заявка отменена");
-  window.location.replace("/");
-});
-
-function updateSelectStyle() {
-  $('select').each(function () {
-    var $this = $(this), numberOfOptions = $(this).children('option').length;
-
-    $this.addClass('select-hidden');
-    $this.wrap('<div class="select"></div>');
-    $this.after('<div class="select-styled"></div>');
-
-    var $styledSelect = $this.next('div.select-styled');
-    $styledSelect.text($this.children('option').eq(0).text());
-
-    var $list = $('<ul />', {
-      'class': 'select-options'
-    }).insertAfter($styledSelect);
-
-    for (var i = 0; i < numberOfOptions; i++) {
-      $('<li />', {
-        text: $this.children('option').eq(i).text(),
-        rel: $this.children('option').eq(i).val()
-      }).appendTo($list);
-      if ($this.children('option').eq(i).is(':selected')) {
-        $('li[rel="' + $this.children('option').eq(i).val() + '"]').addClass('is-selected')
-      }
-    }
-
-    var $listItems = $list.children('li');
-
-    $styledSelect.click(function (e) {
-      e.stopPropagation();
-      $('div.select-styled.active').not(this).each(function () {
-        $(this).removeClass('active').next('ul.select-options').hide();
-      });
-      $(this).toggleClass('active').next('ul.select-options').toggle();
-    });
-
-    $listItems.click(function (e) {
-      e.stopPropagation();
-      $styledSelect.text($(this).text()).removeClass('active');
-      $this.val($(this).attr('rel'));
-      $list.find('li.is-selected').removeClass('is-selected');
-      $list.find('li[rel="' + $(this).attr('rel') + '"]').addClass('is-selected');
-      $list.hide();
-    });
-
-    $(document).click(function () {
-      $styledSelect.removeClass('active');
-      $list.hide();
-    });
-  });
-}
