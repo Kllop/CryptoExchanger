3a2fabd38f97e3987da0b547ccf0a31d9762eb78
Kllop <57048938+Kllop@users.noreply.github.com>
2023-08-11 10:07:56 +0300

    fix

--- Exchanger-data/main.py
+++ Exchanger-data/main.py
@@ -55,8 +55,7 @@ async def status(request: Request):
     jsdata = await request.json()
     resualt = Orders().change_status_order(jsdata.get("key"), jsdata.get("status"))
     if jsdata.get("status") == "payment":
-        order_data = Orders().getOrder(jsdata.get("key"))
-        print(order_data, flush=True)
+        order_data = Orders().getOrder(jsdata.get("key")).get("data")
         TelegramMessage().sendMessage("""Покупатель оплатил ордер № {0} на сумму {1} RUB, переведите {2} в 
                                       количестве {3} на адрес {4}""".format(order_data.get("orderID"), order_data.get("price"), 
                                                                             order_data.get("coin"), order_data.get("count"),
