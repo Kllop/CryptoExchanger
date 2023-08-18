07b862ceeba0a00ce23682ab8fd924bfc84fef03
niruin <87806883+niruin@users.noreply.github.com>
2023-08-18 21:15:21 +0300

    refactor: login logout

--- Web-site/static/js/index.js
+++ Web-site/static/js/index.js
@@ -1,4 +1,6 @@
-$(document).ready(function () {
+const IS_LOGIN_FLAG = 'isLogin';
+
+function assignActiveTab() {
   const path = window.location.pathname;
   if(path === '/') return;
   if(path === '/bid') {
@@ -6,4 +8,89 @@ $(document).ready(function () {
     return
   }
   $(`a[href="${path}"]`).addClass('nav-tab--selected');
+}
+
+function hideNavByIsLogin() {
+  const isLogin = JSON.parse(localStorage.getItem(IS_LOGIN_FLAG));
+  if(isLogin) {
+    $("#header-link-login").hide();
+    $("#header-link-reg").hide();
+    $("#header-link-exit").show();
+  } else {
+    $("#header-link-login").show();
+    $("#header-link-reg").show();
+    $("#header-link-exit").hide();
+  }
+}
+function hideModal(modalElem) {
+  const backdrop = document.querySelector('#modal-backdrop');
+  modalElem.classList.remove('show');
+  backdrop.classList.add('hidden');
+  document.getElementsByTagName('body')[0].style.overflow = 'auto';
+  hideNavByIsLogin()
+}
+
+$(document).ready(function () {
+  assignActiveTab();
+  hideNavByIsLogin();
+  $("#header-link-exit").click(function () {
+    localStorage.removeItem(IS_LOGIN_FLAG);
+    window.location.reload();
+  })
 })
+
+$("#submitLog").on("submit", function(event){
+  event.preventDefault()
+  const login = $("#log-login").val()
+  const password = $("#log-password").val()
+  console.log(login, password)
+  LoginIn(login, password)
+});
+
+$("#submitReg").on("submit", function(event){
+  event.preventDefault()
+  const login = $("#reg-login").val()
+  const password = $("#reg-password").val()
+  const email = $("#reg-email").val()
+  console.log(login, password, email)
+  Registration(login, password, email)
+});
+
+function Registration(login, password, email) {
+  $.ajax({
+    url: "/registration",
+    type: "post",
+    contentType: "application/json; charset=utf-8",
+    data : JSON.stringify({login : login, password : password, email : email}),
+    success: function (response) {
+      localStorage.setItem(IS_LOGIN_FLAG, 'true')
+      hideNavByIsLogin();
+    },
+    error: function (xhr) {
+      alert('Не удалось зарегистрироваться')
+    },
+    complete: function () {
+      hideModal(document.getElementById('modal-registration'))
+    }
+  });
+}
+
+function LoginIn(login, password) {
+  $.ajax({
+    url: "/authorization",
+    type: "post",
+    contentType: "application/json; charset=utf-8",
+    data : JSON.stringify({login : login, password : password}),
+    success: function (response) {
+      localStorage.setItem(IS_LOGIN_FLAG, 'true')
+      hideNavByIsLogin();
+    },
+    error: function (xhr) {
+      alert('Не удалось авторизоваться')
+      console.log("Error load offers")
+    },
+    complete: function () {
+      hideModal(document.getElementById('modal-login'))
+    }
+  });
+}
