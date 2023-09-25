0db3dc535aa765062f79ccf435d9d371d7ae01a8
Kllop <57048938+Kllop@users.noreply.github.com>
2023-09-25 15:00:43 +0300

    fix

--- Exchanger-data/data/course.py
+++ Exchanger-data/data/course.py
@@ -13,6 +13,7 @@ import json
 #
 #IN direction
 #[{"coin" : data[0], "name_exch" : data[1], "name_ru" : data[2], "name_en" : data[3], "name_des" : data[4], "percent" : data [5], "market" : data[6]}]
+
 class Course:
 
     xml_tegs_coins = { "BTC" : "BTC", "USDT" : "USDTERC20", "ETH" : "ETH"}
@@ -33,27 +34,33 @@ class Course:
 
     def __parse_course_xml__(self, preference:list, course:dict) -> dict:
         outdata = """<?xml version="1.0"?><rates>"""
-        for data_pref in preference:
-            jsdata = json.loads(data_pref)
-            coin_name = jsdata['coin']
-            coin_name_out = self.xml_tegs_coins.get(coin_name)
-            bank_name_out = self.xml_tegs_banks.get(jsdata["name_des"])
-            amount = self.xml_tegs_amount.get(coin_name)
-            course_out = course.get("{0}:{1}".format(coin_name, jsdata["name_des"]))
-            outdata += """<item><from>{0}</from><to>{1}</to><in>{2}</in><out>1</out><amount>{3}</amount><minamount>2000 RUB</minamount><maxamount>1000000 RUB</maxamount>
-                          <param>manual</param></item>""".format(bank_name_out, coin_name_out, course_out, amount)
+        try:
+            for data_pref in preference:
+                jsdata = json.loads(data_pref)
+                coin_name = jsdata['coin']
+                coin_name_out = self.xml_tegs_coins.get(coin_name)
+                bank_name_out = self.xml_tegs_banks.get(jsdata["name_des"])
+                amount = self.xml_tegs_amount.get(coin_name)
+                course_out = course.get("{0}:{1}".format(coin_name, jsdata["name_des"]))
+                outdata += """<item><from>{0}</from><to>{1}</to><in>{2}</in><out>1</out><amount>{3}</amount><minamount>2000 RUB</minamount><maxamount>1000000 RUB</maxamount>
+                            <param>manual</param></item>""".format(bank_name_out, coin_name_out, course_out, amount)
+        except Exception as e:
+            print("Error send xml course", e, flush=True)
         return outdata + "</rates>"
     
     def __parse_course__(self, preference:list, course:dict) -> dict:
         outdata = {}
-        for data_pref in preference:
-            jsdata = json.loads(data_pref)
-            coin_name = jsdata['coin']
-            bank_name = jsdata["name_des"]
-            if outdata.get(coin_name) == None:
-                outdata.update({coin_name : {}})
-            coin_data = outdata.get(coin_name)
-            course_data = course.get("{0}:{1}".format(coin_name, bank_name))
-            coin_data.update({bank_name : course_data})
-            outdata.update({coin_name : coin_data})
+        try:
+            for data_pref in preference:
+                jsdata = json.loads(data_pref)
+                coin_name = jsdata['coin']
+                bank_name = jsdata["name_des"]
+                if outdata.get(coin_name) == None:
+                    outdata.update({coin_name : {}})
+                coin_data = outdata.get(coin_name)
+                course_data = course.get("{0}:{1}".format(coin_name, bank_name))
+                coin_data.update({bank_name : course_data})
+                outdata.update({coin_name : coin_data})
+        except Exception as e:
+            print()
         return outdata
